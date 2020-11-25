fc3c6380dd0b862595cce5ca61047446dab3c637
q629988171 <113960096@qq.com>
2020-11-25 15:10:27 +0800

    Update my_watchdog.sh
    增加微信通知功能

--- my_watchdog.sh
+++ my_watchdog.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 #sleep 100
+
 DATE=`date +%Y-%m-%d-%H:%M:%S`
 tries=0
+key=
+
+sc_send(){
+    curl -s "https://sc.ftqq.com/$key.send?text=$1"
+}
 
 echo --- my_watchdog start ---
 while [[ $tries -lt 5 ]]
@@ -13,9 +19,11 @@ do
         if [ ! $state ]
         then
             echo $DATE Ping was successful. >>my_watchdog.log
+            sc_send 网络正常
         elif [ $state == 0 ]
         then
             echo $DATE Ping was successful. >>my_watchdog.log
+            sc_send 网络恢复
         fi
         echo --- exit ---
         echo 1 >my_watchdog.txt
@@ -30,8 +38,10 @@ state=$(cat my_watchdog.txt) >/dev/null 2>&1
 if [ ! $state ]
 then
     echo $DATE Ping had five tries and failed each time. >>my_watchdog.log
+    sc_send 网络故障
 elif [ $state == 1 ]
 then
     echo $DATE Ping had five tries and failed each time. >>my_watchdog.log
+    sc_send 网络故障
 fi
 echo 0 >my_watchdog.txt
