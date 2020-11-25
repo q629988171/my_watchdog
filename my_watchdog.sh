0eba201b901c33d1bf74309d1818b69a2d24284a
q629988171 <113960096@qq.com>
2020-11-25 15:00:13 +0800

    Create my_watchdog.sh
    初始版

---
+++ my_watchdog.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#sleep 100
+
+DATE=`date +%Y-%m-%d-%H:%M:%S`
+tries=0
+
+echo --- my_watchdog start ---
+while [[ $tries -lt 5 ]]
+do
+	if /bin/ping -c 1 8.8.8.8 >/dev/null
+	then
+		echo --- exit ---
+		# echo $DATE OK >>my_watchdog.log
+		exit 0
+	fi
+	tries=$((tries+1))
+	sleep 10
+	# echo $DATE tries: $tries >>my_watchdog.log
+done
+
+echo $DATE network restart >>my_watchdog.log
+/etc/init.d/network restart
+
+#echo $DATE reboot >>my_watchdog.log
+#reboot
